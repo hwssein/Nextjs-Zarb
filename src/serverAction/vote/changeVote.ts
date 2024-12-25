e303fd0f76e50a33ef88d2500effee388b4a9d96
Hossein Mohammadzadeh <4hmzjob@gmail.com>
2024-12-25 16:08:02 +0330

    V.0.4.4.1 fixed deployment bug

--- src/serverAction/vote/changeVote.ts
+++ src/serverAction/vote/changeVote.ts
@@ -1,20 +1,20 @@
-"use server";
+// "use server";
 
-import { FunctionResponse } from "@/types/types";
+// import { FunctionResponse } from "@/types/types";
 
-const changeVote = async (
-  userId: string,
-  musicId: string,
-  voteType: "like" | "dislike"
-): Promise<FunctionResponse> => {
-  try {
-  } catch (error) {
-    if (error instanceof Error) {
-      console.log(error);
-      return { error: error.message };
-    }
-  }
-  return { error: "an unexpected error occurred" };
-};
+// const changeVote = async (
+//   userId: string,
+//   musicId: string,
+//   voteType: "like" | "dislike"
+// ): Promise<FunctionResponse> => {
+//   try {
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       console.log(error);
+//       return { error: error.message };
+//     }
+//   }
+//   return { error: "an unexpected error occurred" };
+// };
 
-export default changeVote;
+// export default changeVote;
